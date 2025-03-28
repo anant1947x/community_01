7e7692574299f910e51457b0c88642dccdf4aa07
anant1947x <anant2006seth@gmail.com>
2025-03-29 00:43:10 +0530

    git hub pr dekho

--- hehe.cpp
+++ hehe.cpp
@@ -2,17 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-     int n; cin>>n;   // root number 
-     int m; cin>>m;   // root of 
-     int a=m/n;
-     vector<int>vec;
-     for (int i=2; i<=a; i++){
-          vec.push_back(i);
-     }
-     int b=vec.size()/2;
-     while (1){
-          if (vec[b])
-     }
+     int n;
 
 
      
