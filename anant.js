dbde250867e49129ed68694d7e2d16e3bfa86fe8
anant1947x <anant2006seth@gmail.com>
2025-03-28 15:13:00 +0530

    acer change

--- anant.js
+++ anant.js
@@ -3,3 +3,4 @@ function anant(a,b){
 }
 console.log("anant")
 console.log(anant(5,6))
+console.log("hello world")
--- hehe.cpp
+++ hehe.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
+     int a=1;
+     int b=2;
 }
